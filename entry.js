dc13a52015db92b6a23c36ba501a509d85e59833
Bart Louwers <bart.louwers@gmail.com>
2025-11-16 11:41:26 +0000

    MapLibre map

--- entry.js
+++ entry.js
@@ -1,1 +1,8 @@
-alert("Hello from JavaScript")
+import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";
+
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://demotiles.maplibre.org/style.json', // style URL
+    center: [0, 0], // starting position [lng, lat]
+    zoom: 1 // starting zoom
+});
